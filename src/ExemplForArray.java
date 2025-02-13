9bcdd5cf4b090af3420718835d868541ca874a14
mmagc <matheusgnann@gmail.com>
2025-02-13 12:41:27 -0300

    Estruturas repetição For Array

---
+++ src/ExemplForArray.java
@@ -0,0 +1,11 @@
+public class ExemplForArray {
+    public static void main(String[] args) {
+
+        String alunos[] = {"Luana", "Matheus", "Gnann", "Galbiatti"};
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("Aluno indice: " + i + " é " + alunos[i]);
+        }
+
+    }
+}
