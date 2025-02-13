c7259e21b7516a0a9993ced939954f6104bfec16
mmagc <matheusgnann@gmail.com>
2025-02-13 10:37:27 -0300

    Estruturas condicição ternarias compostas

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -2,17 +2,8 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota<7 ? "Recuperação" : "Reprovado";
         System.out.println(resultado);
 
-//        if (nota>= 7) {
-//            System.out.println("Aprovado");
-//        } else if (nota>=5 && nota<7) {
-//            System.out.println("Recuperação");
-//        }
-//
-//        else {
-//            System.out.println("Reprovado");
-//        }
     }
 }
