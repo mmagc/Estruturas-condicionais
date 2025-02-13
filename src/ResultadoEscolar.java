16e3dfc5f9cc8f23683cb73379e9c4498722f891
mmagc <matheusgnann@gmail.com>
2025-02-13 10:34:45 -0300

    Estruturas condicição ternaria

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -2,14 +2,17 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        if (nota>= 7) {
-            System.out.println("Aprovado");
-        } else if (nota>=5 && nota<7) {
-            System.out.println("Recuperação");
-        }
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
 
-        else {
-            System.out.println("Reprovado");
-        }
+//        if (nota>= 7) {
+//            System.out.println("Aprovado");
+//        } else if (nota>=5 && nota<7) {
+//            System.out.println("Recuperação");
+//        }
+//
+//        else {
+//            System.out.println("Reprovado");
+//        }
     }
 }
